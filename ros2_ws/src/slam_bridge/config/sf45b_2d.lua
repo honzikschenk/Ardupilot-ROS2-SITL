fa0253809b14d4ee2dfb2dd0d0e4a597e8adc065
honzikschenk <nojschenk@gmail.com>
2025-09-30 19:28:44 -0400

    Translates lua to grab pointcloud data

--- ros2_ws/src/slam_bridge/config/sf45b_2d.lua
+++ ros2_ws/src/slam_bridge/config/sf45b_2d.lua
@@ -5,18 +5,18 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_link",
-  published_frame = "base_link",
+  tracking_frame = "laser_frame",
+  published_frame = "laser_frame",
   odom_frame = "odom",
   provide_odom_frame = true,
   publish_frame_projected_to_2d = false,
   use_odometry = false,
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 1,
+  num_laser_scans = 0,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 0,
+  num_point_clouds = 1,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.5,
   pose_publish_period_sec = 5e-2,
@@ -35,9 +35,19 @@ TRAJECTORY_BUILDER_2D.missing_data_ray_length = 30.0
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
+TRAJECTORY_BUILDER_2D.min_z = -0.2
+TRAJECTORY_BUILDER_2D.max_z = 1.0
+
+TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.025
+TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 0.5
+TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 200
+
 POSE_GRAPH.optimization_problem.huber_scale = 1e1
 POSE_GRAPH.optimize_every_n_nodes = 90
 POSE_GRAPH.constraint_builder.min_score = 0.55
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.6
 
 return options
+
+
+-- ros2 run cartographer_ros cartographer_node -configuration_directory . -configuration_basename sf45b_2d.lua -p provide_odom_frame:=true -p expected_sensor_ids:="[points2]" -r points2:=/pointcloud
